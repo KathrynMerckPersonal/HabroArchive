ea0ee81849538528bf7f09d40524322816f7fe57
KathrynMerckPersonal <catydid919@gmail.com>
2026-02-23 23:58:52 -0500

    commit

--- archive/scripts/data.js
+++ archive/scripts/data.js
@@ -1,3 +1,4 @@
+
 let fileData;
 $.getJSON("scripts/insta.json",
     function (data) {
@@ -18,56 +19,62 @@ class iPost {
     }
 
     createCard(){
+        const pattern = /mp4$/;
         const CId = "carousel" + this.folder;
-        let cardDiv = $(`<div></div>`).addClass("card insta-card order-" + this.index);
-        let cSlide = $(`<div></div>`).addClass("carousel slide").attr("id", CId);
-        // cardDiv.append(cSlide);
-        let carouselI = $(`<div></div>`).addClass("carousel-indicators");
-        let carouselInner = $(`<div></div>`).addClass("carousel-inner");
-        cSlide.append(carouselI, carouselInner);
+        let cardDiv = $(`<div></div>`).addClass("card insta-card");
+        
         let quant = fileData[this.index].Count;
         let urls = fileData[this.index].value; 
-        for(let i = quant; i > 0; i--) {
-            carouselI.append($(`<button></button>`).attr({
+        if(quant == 1 ) {
+            cardDiv.append($("<img>").attr("src", ".." + urls[0]).addClass("card-img-top"));
+        } else {
+            let cSlide = $(`<div></div>`).addClass("carousel slide card-img-top").attr("id", CId);
+            cardDiv.append(cSlide);
+            let carouselI = $(`<div></div>`).addClass("carousel-indicators");
+            let carouselInner = $(`<div></div>`).addClass("carousel-inner");
+            cSlide.append(carouselI, carouselInner);
+            for(let i = 0; i < quant; i++) {
+                carouselI.append($(`<button></button>`).attr({
+                    "type": "button",
+                    "data-bs-target": "#" + CId,
+                    "data-bs-slide-to": i,
+                    "aria-label": "Slide " + (i + 1)
+                }));
+                let ci = $('<div></div>').addClass("carousel-item");
+                if(pattern.test(urls[i])) {
+                    ci.append($("<video></video>").attr({
+                        "src": ".." + urls[i],
+                        "controls": ""
+                    }).addClass("d-block w-100"));
+                } else {
+                    ci.append($("<img>").attr("src", ".." + urls[i]).addClass("d-block w-100"));
+                }
+                console.log(this.index, this.desc, ".." + urls[i]);
+                carouselInner.append(ci);
+                // create button to append
+                // create img to append
+            }
+            carouselI.children().first().addClass("active").attr("aria-current", "true");
+            carouselInner.children().first().addClass("active");
+            cSlide.append($("<button></button>").addClass("carousel-control-prev").attr({
                 "type": "button",
                 "data-bs-target": "#" + CId,
-                "data-bs-slide-to": i,
-                "aria-label": "Slide " + (i + 1)
-            }));
-            let ci = $('<div></div>').addClass("carousel-item");
-            const pattern = /mp4$/;
-            if(pattern.test(urls[i])) {
-                ci.append($("<video></video>").attr({
-                    "src": "../" + urls[i],
-                    "controls": ""
-                }).addClass("d-block w-100"));
-            } else {
-                ci.append($("<img>").attr("src", "../" + urls[i]).addClass("d-block w-100"));
-            }
-            console.log( "../" + urls[i]);
-            carouselInner.append(ci);
-            // create button to append
-            // create img to append
+                "data-bs-slide": "prev"
+            }).append($("<span></span>").addClass("carousel-control-prev-icon").attr("aria-hidden", "true"), 
+            $("<span></span>").addClass("visually-hidden").append("Previous")),
+                $("<button></button>").addClass("carousel-control-next").attr({
+                "type": "button",
+                "data-bs-target": "#" + CId,
+                "data-bs-slide": "next"
+            }).append($("<span></span>").addClass("carousel-control-next-icon").attr("aria-hidden", "true"), 
+            $("<span></span>").addClass("visually-hidden").append("Next")));
         }
-        carouselI.children().last().addClass("active").attr("aria-current", "true");
-        carouselInner.children().last().addClass("active");
-        cSlide.append($("<button></button>").addClass("carousel-control-prev").attr({
-            "type": "button",
-            "data-bs-target": "#" + CId,
-            "data-bs-slide": "prev"
-        }).append($("<span></span>").addClass("carousel-control-prev-icon").attr("aria-hidden", "true"), 
-        $("<span></span>").addClass("visually-hidden").append("Previous")),
-             $("<button></button>").addClass("carousel-control-next").attr({
-            "type": "button",
-            "data-bs-target": "#" + CId,
-            "data-bs-slide": "next"
-        }).append($("<span></span>").addClass("carousel-control-next-icon").attr("aria-hidden", "true"), 
-        $("<span></span>").addClass("visually-hidden").append("Next")));
         //end carousel
         let cbody = $("<div></div>").addClass("card-body").append($("<p></p>").addClass("card-text").append(this.desc));
         cardDiv.append(cbody);        
-        // return cardDiv;
-        return cSlide;
+        let wrapper = $("<div></div>").addClass("col").append(cardDiv);
+        return wrapper;
+        // return cSlide;
     }
 }
 
@@ -83,7 +90,7 @@ Papa.parse(
             for(result of results.data) {
                 const postItem = new iPost(Number(result.index), result.index, result.desc, result.accessed, result.posted);
                 iPostSet.push(postItem);
-                $("#post-container").append(postItem.createCard());
+                $("#post-container").prepend(postItem.createCard());
             }
             console.log(iPostSet);
         },
