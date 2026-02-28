deb600fc461b97b40a60170e3f0c8ec3ef6a75dc
KathrynMerckPersonal <catydid919@gmail.com>
2026-02-27 21:13:43 -0500

    update

--- archive/scripts/base.js
+++ archive/scripts/base.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     
-$(window).scroll(function () { 
-    if($(document).width() <= 576) {
-        $(".header").css({
-            "top": (0 - $(document).scrollTop() / 2) + "px",
-            "opacity": Math.max(1 - $(document).scrollTop() / ($(document).width() * .8), 0)
-        });
-    }
-});
+// $(window).scroll(function () { 
+//     if($(document).width() <= 576) {
+//         $(".header").css({
+//             "top": (0 - $(document).scrollTop() / 2) + "px",
+//             "opacity": Math.max(1 - $(document).scrollTop() / ($(document).width() * .8), 0)
+//         });
+//     }
+// });
 
-$(window).resize(function () { 
-    if($(document).width() > 575) {
-        $(".header").css({"top": 0, "opacity" : 1});
-    }
-});
+// $(window).resize(function () { 
+//     if($(document).width() > 575) {
+//         $(".header").css({"top": 0, "opacity" : 1});
+//     }
+// });
 
 });
