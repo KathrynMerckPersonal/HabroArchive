b5f7275b2bf895dfde6ddc47b6218e43596bb111
KathrynMerckPersonal <catydid919@gmail.com>
2026-03-23 13:22:56 -0400

    Update script.js

--- theorists/theorist-scripts/script.js
+++ theorists/theorist-scripts/script.js
@@ -1,9 +1,21 @@
+let allDat;
+let thisPage;
 
+class Page {
+    constructor(data, tags, user, page) {
+        this.page = page;
+        this.data = data;
+        this.tags = tags;
+        this.user = user;
+    }
+}
 
 function loadContent(user) {
-
+    thisPage = Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vR4PmOLTAzRY1Vp-hpUXwFY0LVrLze68gNYJZYI27aNLzglQGgReVV76gqmYoFxLnPQX9X3Wxvxu_gU/pub?gid=1436726988&single=true&output=csv`, {
+        download: true
+    });
 }
 
 function displayContent(page) {
-
+    
 }
