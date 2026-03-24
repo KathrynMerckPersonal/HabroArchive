5c5700c3517ad59b85c6f0b7e0bf99dbb996b0cf
KathrynMerckPersonal <catydid919@gmail.com>
2026-03-24 00:35:47 -0400

    update

--- theorists/theorist-scripts/script.js
+++ theorists/theorist-scripts/script.js
@@ -1,5 +1,13 @@
 let allDat;
 let thisPage;
+showdown.setOption('optionKeyomitExtraWLInCodeBlocks', 'true');
+showdown.setOption('prefixHeaderId', 'tester');
+showdown.setOption('simplifiedAutoLink', 'true');
+showdown.setOption('excludeTrailingPunctuationFromURLs', 'true');
+showdown.setOption('strikethrough', 'true');
+showdown.setOption('tables', 'true');
+showdown.setOption('tasklists', 'true');
+showdown.setOption('requireSpaceBeforeHeadingText', 'true');
 
 class Page {
     constructor(data, tags, user, page) {
@@ -10,12 +18,37 @@ class Page {
     }
 }
 
-function loadContent(user) {
-    thisPage = Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vR4PmOLTAzRY1Vp-hpUXwFY0LVrLze68gNYJZYI27aNLzglQGgReVV76gqmYoFxLnPQX9X3Wxvxu_gU/pub?gid=1436726988&single=true&output=csv`, {
-        download: true
+function loadContent(user, page) {
+    Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vR4PmOLTAzRY1Vp-hpUXwFY0LVrLze68gNYJZYI27aNLzglQGgReVV76gqmYoFxLnPQX9X3Wxvxu_gU/pub?gid=1436726988&single=true&output=csv`, {
+        download: true,
+        complete: function(results) {
+            thisPage = results.data[0][0];
+            console.log(thisPage);
+            displayContent(page);
+        }
     });
 }
 
 function displayContent(page) {
-    
+    let converter = new showdown.Converter({extensions: [kathrynsBS]});
+    let html = converter.makeHtml(thisPage);
+    $("#imported-content").append(html);
+    console.log("displayed");
+}
+
+function kathrynsBS() {
+    let hpEmotes = {
+
+    };
+    let escapeChars = {
+        type: 'lang',
+        regex: /</g,
+        replace: '&lt;'
+    };
+    let hashStyle = {
+        type: 'output',
+        regex: /<(.*)>#(\w+)/g,
+        replace: '<$1 class="$2">'
+    };
+    return[escapeChars, hashStyle];
 }
