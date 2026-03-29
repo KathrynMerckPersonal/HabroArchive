4a26f4678c308b17f599c1c560546ad23096d9b8
KathrynMerckPersonal <catydid919@gmail.com>
2026-03-29 16:43:22 -0400

    boys, we saved the city

--- theorists/theorist-scripts/livedemo.js
+++ theorists/theorist-scripts/livedemo.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     
     let inp = $("#inputcont");
     let oup = $("#outputcont");
-
+    inp.autoResize();
     $(inp).on('input', function () { 
         oup.html(converter.makeHtml(inp.val()));
         
