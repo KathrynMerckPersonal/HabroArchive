135ee8c1635192e939a22b063ab72069e4cbb261
KathrynMerckPersonal <catydid919@gmail.com>
2026-04-03 12:06:19 -0400

    working on shit

--- theorists/theorist-scripts/script.js
+++ theorists/theorist-scripts/script.js
@@ -12,17 +12,23 @@ class Page {
 }
 
 function loadContent(user, page) {
-    Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vR4PmOLTAzRY1Vp-hpUXwFY0LVrLze68gNYJZYI27aNLzglQGgReVV76gqmYoFxLnPQX9X3Wxvxu_gU/pub?gid=1436726988&single=true&output=csv`, {
+    Papa.parse(`https://docs.google.com/spreadsheets/d/e/2PACX-1vR4PmOLTAzRY1Vp-hpUXwFY0LVrLze68gNYJZYI27aNLzglQGgReVV76gqmYoFxLnPQX9X3Wxvxu_gU/pub?gid=0&single=true&output=csv`, {
         download: true,
+        header: true,
         complete: function(results) {
-            thisPage = results.data[0][0];
+            thisPage = results.data;
             displayContent(page);
         }
     });
 }
 
 function displayContent(page) {
-    let html = converter.makeHtml(thisPage);
-    $("#imported-content").append(html);
-    console.log("displayed");
+    if(page == "toc") {
+        console.log(thisPage)
+    } else {
+        let html = converter.makeHtml(thisPage);
+        $("#imported-content").append(html);
+        console.log("displayed");
+    }
+    
 }
